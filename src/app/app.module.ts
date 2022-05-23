20328f08537ff56c652bc1e8991de4eaa9f3538d
borilio <exprimetuandroid@gmail.com>
2022-05-23 12:04:30 +0200

    Big bang

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+//Módulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { EstructuraModule } from './estructura/estructura.module';
+
+//Componentes
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EstructuraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/estructura/estructura.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { CuerpoComponent } from './components/cuerpo/cuerpo.component';
+import { PieComponent } from './components/pie/pie.component';
+import { PagePrincipalComponent } from './components/page-principal/page-principal.component';
+
+
+@NgModule({
+  declarations: [
+    CabeceraComponent,
+    CuerpoComponent,
+    PieComponent,
+    PagePrincipalComponent
+  ],
+  exports : [
+    PagePrincipalComponent
+  ], 
+  imports: [
+    CommonModule
+  ]
+})
+export class EstructuraModule { }
