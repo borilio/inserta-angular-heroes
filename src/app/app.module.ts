42e0d0ddfcbd2fce14ebf3283a9f0230e863b573
borilio <exprimetuandroid@gmail.com>
2022-05-25 13:20:31 +0200

    Añadido servicios (creación y uso). También añadida la columna de las imágenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { EstructuraModule } from './estructura/estructura.module';
+import { SharedModule } from './shared/shared.module';
 
 //Componentes
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    EstructuraModule
+    EstructuraModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
